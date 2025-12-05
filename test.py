86eeb3c7136cfb4dc3753bdfe2b7c6afcf5ceb70
Jason-8577 <jasono119@nycstudents.net>
2025-12-05 06:25:41 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import requests
+""" import requests
 
 def getPoke(poke):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
@@ -16,4 +16,22 @@ def getPoke(poke):
 
 pokemon = getPoke("Charizard")
 for key, value in pokemon.items():
+    print(f"{key.title()}: {value}")"""
+
+
+import requests
+
+def info(search):
+    response = requests.get(f"https://www.dnd5eapi.co/api/2014/{search.lower()}")
+    if response.status_code != 400:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return{
+        "name": data["name"]
+    }
+dnd = info("acid arrow")
+for key, value in dnd.items():
     print(f"{key.title()}: {value}")
+    
