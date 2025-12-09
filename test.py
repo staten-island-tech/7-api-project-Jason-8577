f216eb4ace72bd09957cc02b52aa1466c7e7d74e
Jason-8577 <jasono119@nycstudents.net>
2025-12-09 06:25:35 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-""" import requests
+""" https://opentdb.com/api_config.php """
+
+import requests
 
 def getPoke(poke):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
@@ -16,9 +18,20 @@ def getPoke(poke):
 
 pokemon = getPoke("Charizard")
 for key, value in pokemon.items():
-    print(f"{key.title()}: {value}") """
+    print(f"{key.title()}: {value}")
     
 
+import requests
 
-
-https://opentdb.com/api_config.php
+def trivia_questions(questions):
+    response = requests.get(f"https://opentdb.com/api.php?amount=10&category=15&type=multiple{questions.lower()}") 
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+        
+    data = response.json()
+    return data
+    
+trivia = trivia_questions("easy")
+print(trivia)
+    
