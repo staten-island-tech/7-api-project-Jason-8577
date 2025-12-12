6b5cd3e3b4f8203d355ec2a4896dcdcc99f9f521
Jason-8577 <jasono119@nycstudents.net>
2025-12-12 06:22:24 -0800

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ for key, value in pokemon.items():
     print(f"{key.title()}: {value}") """
 
 
-""" import requests
+import requests
 
 def getclass(Class):
     response = requests.get(f"https://www.dnd5eapi.co/api/2014/classes/{Class.lower()}")
@@ -30,60 +30,37 @@ def getclass(Class):
     data = response.json()
     return data
 
-dnd = getclass("Druid")
-for key, value in dnd.items():
-    print(f"{key.title()}: {value}")
-
-
-import requests
-
-def getclass(Class):
-    response = requests.get(f"https://www.dnd5eapi.co/api/2014/classes/{Class.lower()}")
-    if response.status_code != 200:
-        print("Error")
-        return None
-    
-    data = response.json()
-    return {
-        "index": data["index"],
-        "name": data["name"],
-        "url": data["url"]
-    }
-
-dnd = getclass("barbarian")
-for key, value in dnd.items():
-    print(f"{key.title()}: {value}") """
-
-"""     https://www.dnd5eapi.co/ """
-
-
+b = getclass("Barbarian")
+B = getclass("Bard")
+c = getclass("Cleric")
+d = getclass("Druid")
+f = getclass("Fighter")
+m = getclass("Monk")
+p = getclass("Paladin")
+r = getclass("Ranger")
+R = getclass("Rogue")
+s = getclass("Sorcerer")
+w = getclass("Warlock")
+W = getclass("Wizard")
 
 import tkinter as tk
 
+def imports():
+    for key, value in W.items():              #Change the () or letter according to class {for key, value in ().items():}
+        print(f"{key.title()}: {value}")
+
 window = tk.Tk()
-window.title("Message Reverser")
-window.geometry("400x250")
-window.resizable(False, False)
+window.title("Dnd")
+my_button = tk.Button(
+window, 
+text = "Class imports",
+command = imports,
+font = ("Arial", 16),
+bg = "darkblue",
+fg = "white",
+relief = "raised",
+padx = 10, pady = 5
+)
 
-prompt = tk.Label(window, text="Type your message below:",
-font=("Arial", 14))
-prompt.pack(pady=10)
-
-entry = tk.Entry(window, font=("Arial", 14), width=30)
-entry.pack(pady=5)
-
-result_label = tk.Label(window, text="", font=("Arial", 14, "bold"),
-fg="blue")
-result_label.pack(pady=15)
-
-def reverse_message():
-    text = entry.get()
-    reversed_text = text[::-1]
-    result_label.config(text=f"Backwards: {reversed_text}")
-
-reverse_button = tk.Button(window, text="Reverse Message!",
-font=("Arial", 14),
-
-command=reverse_message)
-
-reverse_button.pack(pady=10)
+my_button.pack(pady=20)
+window.mainloop()
